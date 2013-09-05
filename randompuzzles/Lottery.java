a6bbe1bec738993b8300f58bbad6123a0f31e1f7
ashish173 <aitashish173@gmail.com>
2013-09-05 15:38:53 +0530

    added Lottery problem

---
+++ randompuzzles/Lottery.java
@@ -0,0 +1,23 @@
+// Lottery.java 
+// problem statement
+// Class:
+// Lottery
+// Method:
+// sortByOdds
+// Parameters:
+// String[]
+// Returns:
+// String[]
+// Method signature:
+// String[] sortByOdds(String[] rules)
+// (be sure your method is public)
+//     
+
+public class Lottery {
+    
+    
+    public static void main(String args[]) {
+
+    } 
+
+}
