d099d3e7561b0f849e19603b32e33ffe301881bd
ashish173 <aitashish173@gmail.com>
2013-09-11 08:12:13 +0530

    corrected Sentence formation code

--- randompuzzles/SentenceFormation.java
+++ randompuzzles/SentenceFormation.java
@@ -2,16 +2,21 @@
 //
 public class SentenceFormation {
     static String[] dict = {"swim","swimmed","swimming","edge","gem","met","etopia","opia","arms", "topi", "rms"};
+    // "rms", "arms" are present in our dictionary
+    
     static String message = "swimmedgemetopiarms";
     static int messageLength = message.length();
     static boolean sentenceFormationFlag = false;   // checks for last word in the message is in dictionary
     static String[] decipheredMessage;
     static int pointer = 0;
+    
+    // main driver function 
     public static void main(String[] args) {
     	// System.out.println("this is main");
     	makeSentence(0); // passing index of message to the makeSentence
   	} 
 
+  	// recursive function to break the message
   	public static boolean makeSentence(int index) {
   		if(index==messageLength-1 && sentenceFormationFlag==true) { // base case 
   			// end of message and sentenceFormation flag True
@@ -38,16 +43,16 @@ public class SentenceFormation {
   					// System.out.println("make sentence called for word " + word);
   					makeSentence(i+1);
   					if(sentenceFormationFlag==true) {
-  						// System.out.println(" " + word);
+  						System.out.println(" " + word);
   						// add the words to an array of strings
-  						decipheredMessage[pointer] = new String();
-  						decipheredMessage[pointer++] = word;
+  						// decipheredMessage[pointer] = new String();
+  						// decipheredMessage[pointer++] = word;
   						return true;
   					}
 				}
   			}
   		}
-  		showMessage(decipheredMessage);
+  		// showMessage(decipheredMessage);
   		return true;
   	}
 
