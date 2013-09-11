09eaee3db667d5dbf0884ddf275961bf8cb80dc5
ashish173 <aitashish173@gmail.com>
2013-09-11 08:07:00 +0530

    Sentence formation problem

---
+++ randompuzzles/SentenceFormation.java
@@ -0,0 +1,71 @@
+// SentenceFormation.java
+//
+public class SentenceFormation {
+    static String[] dict = {"swim","swimmed","swimming","edge","gem","met","etopia","opia","arms", "topi", "rms"};
+    static String message = "swimmedgemetopiarms";
+    static int messageLength = message.length();
+    static boolean sentenceFormationFlag = false;   // checks for last word in the message is in dictionary
+    static String[] decipheredMessage;
+    static int pointer = 0;
+    public static void main(String[] args) {
+    	// System.out.println("this is main");
+    	makeSentence(0); // passing index of message to the makeSentence
+  	} 
+
+  	public static boolean makeSentence(int index) {
+  		if(index==messageLength-1 && sentenceFormationFlag==true) { // base case 
+  			// end of message and sentenceFormation flag True
+  			return true;
+  		}
+  		else if(index==messageLength-1 && sentenceFormationFlag==false) {  // another base case
+  			// end of message and sentenceFormation flag False	
+  			return false;
+  		}
+  		else { // final condition
+  			String word="";
+  			for(int i=index; i<messageLength; i++) {
+  				word = word + message.charAt(i);
+  				// System.out.println("word is " + word);
+
+  				if(search(word) && i==messageLength-1) {
+  					// System.out.println("made the sentence " + word);
+  					// makeSentence(i+1);
+  					sentenceFormationFlag=true;
+  					System.out.println(" " + word);	
+  					return true;
+  				}
+				else if(search(word)) {
+  					// System.out.println("make sentence called for word " + word);
+  					makeSentence(i+1);
+  					if(sentenceFormationFlag==true) {
+  						// System.out.println(" " + word);
+  						// add the words to an array of strings
+  						decipheredMessage[pointer] = new String();
+  						decipheredMessage[pointer++] = word;
+  						return true;
+  					}
+				}
+  			}
+  		}
+  		showMessage(decipheredMessage);
+  		return true;
+  	}
+
+  	public static boolean search(String key) {
+  		for(int i=0; i<dict.length; i++) {
+  			if(dict[i].equals(key)) {
+  				// System.out.println("got a word in dict");
+  				return true;
+  			}
+  		}
+  		return false;
+  	}
+
+  	public static void showMessage(String[] decipheredMessage) {
+  		if(decipheredMessage != null) {
+  			for(int i=decipheredMessage.length; i>=0; i++)
+  				System.out.println(" " + decipheredMessage[i]);
+  		}
+  	}
+
+}
